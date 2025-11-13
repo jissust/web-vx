d810b25493a1cdac14f57dd763c933751678eca1
jesustissera <jesustissera@gmail.com>
2025-11-13 15:51:50 -0300

    funcionalidad para realizar el conteo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Carrusel
+ */
 const swiper_carousel_1 = new Swiper(
   ".yes-we-stack-like-pros-swiper__carousel_1",
   {
@@ -69,3 +72,29 @@ const swiper_carousel_2 = new Swiper(
     },
   }
 );
+
+/**
+ * Count
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".counter");
+
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const speed = counter.getAttribute("data-speed") || 200;
+      const target = +counter.getAttribute("data-target");
+      const index = +counter.getAttribute("data-index");
+      const count = +counter.innerText;
+      const increment = target / speed;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCount, 20); //
+      } else {
+        counter.innerText = `+${target.toLocaleString()}${index === 3 ? 'K' : ''}`; // formato con comas o puntos
+      }
+    };
+
+    updateCount();
+  });
+});
