4e5b55dae05cfc90914418bb4775c24662157b4f
jesustissera <jesustissera@gmail.com>
2025-11-21 10:55:12 -0300

    animacion para texto

--- js/script.js
+++ js/script.js
@@ -226,3 +226,26 @@ const validateEmail = (email) => {
   const regex = /^[^@]+@[^@]+\.[^@]+$/;
   return regex.test(email.trim());
 };
+
+/**
+ * Change words
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const items = document.querySelectorAll(".hero_h1_item");
+  let index = 0;
+
+  setInterval(() => {
+    // remover active del actual
+    items[index].classList.remove("active");
+
+    // pasar al siguiente
+    index = (index + 1) % items.length;
+
+    // activar el siguiente
+    items[index].classList.add("active");
+
+  }, 3000); // cada 3 segundos
+
+});
