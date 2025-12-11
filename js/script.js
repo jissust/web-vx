2dc8ca71f61b0e222ebc7b137957eef1dbfc81c6
jesustissera <jesustissera@gmail.com>
2025-12-11 11:22:41 -0300

    Menú: existia un error con swiper

--- js/script.js
+++ js/script.js
@@ -1,79 +1,4 @@
 /**
- * Carrusel
- */
-const swiper_carousel_1 = new Swiper(
-  ".yes-we-stack-like-pros-swiper__carousel_1",
-  {
-    loop: true,
-    freeMode: true,
-    spaceBetween: 0,
-    grabCursor: true,
-    slidesPerView: 1,
-    loop: true,
-    autoplay: {
-      delay: 1,
-      disableOnInteraction: true,
-    },
-    freeMode: true,
-    speed: 5000,
-    freeModeMomentum: false,
-    allowTouchMove: false,
-    grabCursor: false,
-    breakpoints: {
-      320: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-      480: {
-        slidesPerView: 5,
-        spaceBetween: 20,
-      },
-      992: {
-        slidesPerView: 10,
-        spaceBetween: 0,
-      },
-    },
-  }
-);
-
-const swiper_carousel_2 = new Swiper(
-  ".yes-we-stack-like-pros-swiper__carousel_2",
-  {
-    loop: true,
-    freeMode: true,
-    spaceBetween: 0,
-    grabCursor: true,
-    slidesPerView: 1,
-    loop: true,
-    autoplay: {
-      delay: 1,
-      disableOnInteraction: false,
-      reverseDirection: true,
-      pauseOnMouseEnter: false,
-    },
-    freeMode: true,
-    speed: 5000,
-    freeModeMomentum: false,
-    allowTouchMove: false,
-    grabCursor: false,
-    breakpoints: {
-      320: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-      480: {
-        slidesPerView: 5,
-        spaceBetween: 20,
-      },
-      992: {
-        slidesPerView: 10,
-        spaceBetween: 0,
-      },
-    },
-  }
-);
-
-/**
  * Count
  */
 document.addEventListener("DOMContentLoaded", () => {
@@ -227,7 +152,6 @@ const validateEmail = (email) => {
   return regex.test(email.trim());
 };
 
-
 document.addEventListener("DOMContentLoaded", () => {
   // 1. Seleccionar todos los enlaces del menú (nav-link y dropdown-item)
   const menuLinks = document.querySelectorAll(".nav-link, .dropdown-item, .navbar-brand");
@@ -271,3 +195,78 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+/**
+ * Carrusel
+ */
+const swiper_carousel_1 = new Swiper(
+  ".yes-we-stack-like-pros-swiper__carousel_1",
+  {
+    loop: true,
+    freeMode: true,
+    spaceBetween: 0,
+    grabCursor: true,
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: true,
+    },
+    freeMode: true,
+    speed: 5000,
+    freeModeMomentum: false,
+    allowTouchMove: false,
+    grabCursor: false,
+    breakpoints: {
+      320: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      480: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 10,
+        spaceBetween: 0,
+      },
+    },
+  }
+);
+
+const swiper_carousel_2 = new Swiper(
+  ".yes-we-stack-like-pros-swiper__carousel_2",
+  {
+    loop: true,
+    freeMode: true,
+    spaceBetween: 0,
+    grabCursor: true,
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: false,
+      reverseDirection: true,
+      pauseOnMouseEnter: false,
+    },
+    freeMode: true,
+    speed: 5000,
+    freeModeMomentum: false,
+    allowTouchMove: false,
+    grabCursor: false,
+    breakpoints: {
+      320: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      480: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 10,
+        spaceBetween: 0,
+      },
+    },
+  }
+);
